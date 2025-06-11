557b26bde2ae3e2ae1783ac6d667993f1bb2e0b9
nthung1021 <nthung22@clc.fitus.edu.vn>
2025-06-11 11:26:41 +0700

    Create folders for backend and frontend code

--- source/backend/app.js
+++ source/backend/app.js
@@ -3,7 +3,12 @@ const bookRoutes = require('./interfaces/routes/bookRoutes.js');
 
 const app = express();
 app.use(express.json());
-app.use('/api/books', bookRoutes);
+app.use('/api/book', bookRoutes);
+
+app.use(cors({
+    origin: process.env.WEB_URL,
+    credentials: true
+}));
 
 const port = process.env.PORT || 3000;
 
